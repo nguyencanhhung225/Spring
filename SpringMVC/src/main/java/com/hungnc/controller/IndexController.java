9b253610e5d43e7a3d020628208ed4a969cf8030
Nguyen Canh Hung <nguyencanhhung225@gmail.com>
2017-04-30 16:07:56 -0700

    init and config project spring

---
+++ SpringMVC/src/main/java/com/hungnc/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.hungnc.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+	
+	@RequestMapping(value = "/index")
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+	
+}
